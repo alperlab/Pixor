c14de7811ff7fb2b2605dfff6337541d0020063b
alperlab <alperalakir@gmail.com>
2026-06-24 22:04:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -49,3 +49,49 @@ JSON.stringify(pixorSettings)
 }
 
 loadSettings();
+// =====================
+// PIXOR LIVE WEATHER
+// =====================
+
+async function loadWeather(){
+
+try{
+
+const response = await fetch(
+"https://api.open-meteo.com/v1/forecast?latitude=41.01&longitude=28.97&current=temperature_2m"
+);
+
+const data = await response.json();
+
+const temp =
+Math.round(
+data.current.temperature_2m
+);
+
+const weatherBox =
+document.querySelector(".weatherTemp");
+
+if(weatherBox){
+
+weatherBox.innerHTML =
+temp + "°C";
+
+}
+
+}catch(error){
+
+console.log(
+"Weather Error",
+error
+);
+
+}
+
+}
+
+loadWeather();
+
+setInterval(
+loadWeather,
+300000
+);
