9e649a6bf88507d0530110d1c85893f90ce6f651
alperlab <alperalakir@gmail.com>
2026-06-24 21:43:33 +0300

    Create widgets.js

---
+++ widgets.js
@@ -0,0 +1,57 @@
+const widgets = [
+
+{
+  id:"clock",
+  type:"clock",
+  title:"Saat"
+},
+
+{
+  id:"weather",
+  type:"weather",
+  title:"Hava"
+},
+
+{
+  id:"photo",
+  type:"photo",
+  title:"Fotoğraf"
+},
+
+{
+  id:"crypto",
+  type:"crypto",
+  title:"BTC"
+},
+
+{
+  id:"news",
+  type:"news",
+  title:"Haber"
+},
+
+{
+  id:"calendar",
+  type:"calendar",
+  title:"Takvim"
+},
+
+{
+  id:"spotify",
+  type:"spotify",
+  title:"Spotify"
+},
+
+{
+  id:"notes",
+  type:"notes",
+  title:"Notlar"
+},
+
+{
+  id:"currency",
+  type:"currency",
+  title:"Döviz"
+}
+
+];
