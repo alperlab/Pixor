51c7fe9e04d3072bc95870bc67f53aa566625fc3
alperlab <alperalakir@gmail.com>
2026-06-29 17:51:35 +0300

    Refactor widgets structure and add console log

--- widgets.js
+++ widgets.js
@@ -1,57 +1,11 @@
-const widgets = [
+console.log("Widgets loaded");
 
-{
-  id:"clock",
-  type:"clock",
-  title:"Saat"
-},
+const Widgets = {
 
-{
-  id:"weather",
-  type:"weather",
-  title:"Hava"
-},
+    refresh() {
 
-{
-  id:"photo",
-  type:"photo",
-  title:"Fotoğraf"
-},
+        console.log("Refreshing widgets...");
 
-{
-  id:"crypto",
-  type:"crypto",
-  title:"BTC"
-},
+    }
 
-{
-  id:"news",
-  type:"news",
-  title:"Haber"
-},
-
-{
-  id:"calendar",
-  type:"calendar",
-  title:"Takvim"
-},
-
-{
-  id:"spotify",
-  type:"spotify",
-  title:"Spotify"
-},
-
-{
-  id:"notes",
-  type:"notes",
-  title:"Notlar"
-},
-
-{
-  id:"currency",
-  type:"currency",
-  title:"Döviz"
-}
-
-];
+};
