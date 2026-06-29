7638b9aa89454ac0453c3ede99915d3270c1e64d
alperlab <alperalakir@gmail.com>
2026-06-29 17:49:54 +0300

    Implement stock loading and display functionality
    Added a function to load and display stock prices every minute.

--- services/stocks.js
+++ services/stocks.js
@@ -1,1 +1,36 @@
-console.log("Stocks Service Loaded");
+console.log("Stocks service loaded");
+
+async function loadStocks() {
+
+    try {
+
+        // Şimdilik örnek veriler.
+        // Daha sonra gerçek API bağlayacağız.
+
+        const stocks = {
+            TSLA: "403.31",
+            NVDA: "133.23",
+            AAPL: "234.40"
+        };
+
+        const stockBox = document.querySelector(".stock");
+
+        if (!stockBox) return;
+
+        stockBox.innerHTML = `
+            <div>TSLA ${stocks.TSLA}</div>
+            <div>NVDA ${stocks.NVDA}</div>
+            <div>AAPL ${stocks.AAPL}</div>
+        `;
+
+    } catch (err) {
+
+        console.error(err);
+
+    }
+
+}
+
+loadStocks();
+
+setInterval(loadStocks,60000);
